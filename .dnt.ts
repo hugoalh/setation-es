b274991494d83f37bc465d0f674fb49adb8def13
hugoalh <hugoalh@gmail.com>
2025-02-11 17:55:14 +0800

    Update resources

--- .dnt.ts
+++ .dnt.ts
@@ -9,6 +9,7 @@ await invokeDenoNodeJSTransformer({
 		"README.md"
 	],
 	entrypoints: configJSR.getExports(),
+	fixInjectedImports: true,
 	generateDeclarationMap: true,
 	mappings: {
 		"https://raw.githubusercontent.com/hugoalh/sort-es/v0.1.1/numerics.ts": {
